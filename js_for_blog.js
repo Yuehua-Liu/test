6a8b92385d942665faf2a76d0f8b894fd6aaffe4
劉岳樺 <ffang55tw@gmail.com>
2015-11-27 13:13:24 +0800

    first version

--- js_for_blog.js
+++ js_for_blog.js
@@ -22,19 +22,25 @@ $(document).ready(function(){
 	// 	html += '</div>';
 	// 	$(".article").append(html);
 	// }
-	//預設讀取"全部"類文章
+	
+	// 預設讀取"全部"類文章
 	$.getJSON('https://www.dcard.tw/api/forum/all/1',function(data){
 		
 		$(".article").empty();
 
 		$.each(data,function(entryIndex, entry){
-			// console.log(entry);
-			var html = '<div class="entry">';
-			html += '<h3 class="title">'+ entry['title'] + '</h3>';
+			// console.log(entry['version'][0].title);
+			var html = '<div id="article_position">';
+			html += '<a class="article_block" href="#">';
+			html += '<span class="article">';
+			html += '<h3 class="title">'+ entry['version'][0].title + '</h3>';
+			html += '<hr>';
 			html += '<h4 class="type">' + "分類: " + entry['forum_name'] + '</h4>';
 			html += '<h4 class="type">' + "人氣: " + entry['like'] + '</h4>';
+			html += '</span>';
+			html += '</a>';
 			html += '</div>';
-			$(".article").append(html);
+			$("#article_position").append(html);
 		});
 	});
 });
